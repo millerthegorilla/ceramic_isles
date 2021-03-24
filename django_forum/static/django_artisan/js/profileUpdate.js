bb8f7db2ca1b352983280b39fe1f1b675455fa54
james miller <jamesstewartmiller@gmail.com>
2021-03-24 20:57:59 +0000

    removed static dir from git

--- django_forum/static/django_artisan/js/profileUpdate.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function () {
-    $("#avatarImage").click(function(e) {
-        $("#avatarUpload").click();
-    });
-    $('#avatarUpload').on("change", function( event ) {
-        $("#avatarform").submit();
-    });
-});
