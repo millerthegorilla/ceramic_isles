a5d19b27245fe9add0a62603f19150dd5c8f5a96
james miller <jamesstewartmiller@gmail.com>
2021-03-28 23:43:08 +0100

    changed top level from django_forum to django_artisan

--- django_forum/django_artisan/static/django_artisan/js/carousel.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function () {
-            $(".carousel-item").hover(function(){ $(".carousel-caption").hide();
-              $(".carousel-caption").css('visibility', 'visible');
-              $(".carousel-caption").stop().fadeIn(1000) },
-              function(){   $(".carousel-caption").stop().fadeOut(800, function() {
-                            $(".carousel-caption").css('visibility', 'hidden'); }) });
-        });
