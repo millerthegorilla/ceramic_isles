3d8be7dac3d0b4be3f11b07cebce039bd0ad71f6
James Miller <jamesstewartmiller@gmail.com>
2021-04-24 10:13:58 +0100

    debugging, and setting up debug properties in settings/urls etc

--- django_artisan/ceramic_isles/urls.py
+++ django_artisan/ceramic_isles/urls.py
@@ -36,10 +36,15 @@ urlpatterns = [
     path('tinymce/', include('tinymce.urls')),
 ]
 
-if True:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-    import debug_toolbar
-    urlpatterns = [
-        path('__debug__/', include(debug_toolbar.urls)),
-    ] + urlpatterns
+try:
+    settings.DEBUG
+except NameError:
+    pass # log that we are not debug
+else:
+    if settings.DEBUG == True:
+        urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+        urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+        import debug_toolbar
+        urlpatterns = [
+            path('__debug__/', include(debug_toolbar.urls)),
+        ] + urlpatterns
--- django_artisan/django_profile/models.py
+++ django_artisan/django_profile/models.py
@@ -8,13 +8,17 @@ from django.conf import settings
 from django.contrib.contenttypes.models import ContentType
 
 
+def get_default_display_name():
+    return str(uuid.uuid4())
+
+
 # Create your models here.
 class Profile(models.Model):
     """
         user profile
     """
     profile_user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
-    display_name = models.CharField(max_length=37, blank=True, unique=True, default=str(uuid.uuid4()))
+    display_name = models.CharField(max_length=37, blank=True, unique=True, default=get_default_display_name())
 
     def __str__(self):
         return str(self._meta.get_fields(include_hidden=True))
