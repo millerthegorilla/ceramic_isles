575848d072f6c44e3b83e78eb2645b8fdf023e1b
James Miller <jamesstewartmiller@gmail.com>
2021-05-03 20:15:05 +0100

    updated included files

--- django_artisan/ceramic_isles/wsgi.py
+++ django_artisan/ceramic_isles/wsgi.py
@@ -12,7 +12,7 @@ import os
 from django.core.wsgi import get_wsgi_application
 
 from dotenv import load_dotenv
-project_folder = os.path.expanduser('/etc/opt/ceramic_isles/settings/')  # adju$
+project_folder = os.path.expanduser('/etc/opt/ceramic_isles_staging/settings/')
 load_dotenv(os.path.join(project_folder, '.env'))
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'settings')
--- django_artisan/manage.py
+++ django_artisan/manage.py
@@ -8,7 +8,7 @@ def main():
     """Run administrative tasks."""
 
     from dotenv import load_dotenv
-    project_folder = os.path.expanduser('/etc/opt/ceramic_isles_dev/settings/')  # adju$
+    project_folder = os.path.expanduser('/etc/opt/ceramic_isles_staging/settings/')  # adju$
     load_dotenv(os.path.join(project_folder, '.env'))
 
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'settings')
