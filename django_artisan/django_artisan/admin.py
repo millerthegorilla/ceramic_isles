dd2ad3d3f79c829c4aa03af4165d9899477d25fb
james miller <jamesstewartmiller@gmail.com>
2021-08-31 11:05:29 +0100

    before running autopep8

--- django_artisan/django_artisan/admin.py
+++ django_artisan/django_artisan/admin.py
@@ -42,39 +42,66 @@ class DjangoArtisanConfig(AppConfig):
 
 @admin.register(UserProductImage)
 class ImageAdmin(admin.ModelAdmin):
-    list_display = ('active', 'image_file', 'image_shop_link', 'image_title', 'image_text', 'image_shop_link_title')
-    list_filter = ('active', 'image_file', 'image_title')
-    search_fields = ('image_text', 'image_title', 'image_shop_link')
+    list_display = (
+        'active', 'image_file',
+        'image_shop_link',
+        'image_title', 'image_text',
+        'image_shop_link_title'
+    )
+    list_filter = (
+        'active', 'image_file',
+        'image_title'
+    )
+    search_fields = (
+        'image_text', 'image_title',
+        'image_shop_link'
+    )
     actions = ['approve_image']
 
     def approve_image(self, request, queryset):
         updated = queryset.update(active=True)
-        self.message_user(request, ngettext(
+        self.message_user(
+            request, 
+            ngettext(
                     '%d image was approved.',
                     '%d images were approved.',
                     updated,
-                ) % updated, messages.SUCCESS)
+            ) % updated, 
+            messages.SUCCESS
+        )
 
 @admin.register(Event)
 class EventAdmin(admin.ModelAdmin):
-    list_display = ('active', 'title', 'text', 'date', 'repeating')
+    list_display = (
+        'active', 'title',
+        'text', 'date',
+        'repeating'
+    )
     actions = ['approve_event', 'disapprove_event']
 
     def approve_event(self, request, queryset):
         updated = queryset.update(active=True)
-        self.message_user(request, ngettext(
+        self.message_user(
+            request,
+            ngettext(
                     '%d event was approved.',
                     '%d events were approved.',
                     updated,
-                ) % updated, messages.SUCCESS)
+            ) % updated, 
+            messages.SUCCESS
+        )
 
     def disapprove_event(self, request, queryset):
         updated = queryset.update(active=False)       
-        self.message_user(request, ngettext(
+        self.message_user(
+            request, 
+            ngettext(
                     '%d event was disapproved.',
                     '%d events were disapproved.',
                     updated,
-                ) % updated, messages.SUCCESS)
+            ) % updated, 
+            messages.SUCCESS
+        )
 
 # #admin.site.unregister(UserPasswordHistoryConfig)
 admin.site.unregister(PasswordHistory)
@@ -84,13 +111,24 @@ admin.site.unregister(ForumProfile)
 # Register your models here.
 @admin.register(ArtisanForumProfile)
 class ArtisanForumProfileAdmin(admin.ModelAdmin):
-    list_display = ['display_name', 'bio', 'image_file', 'shop_web_address', 
-                    'outlets', 'listed_member', 'display_personal_page', 
-                    'address_line_1','address_line_2','parish','postcode',
-                    'avatar','rules_agreed']
-    list_filter = ['display_name','parish','rules_agreed', 'shop_web_address',
-                   'listed_member', 'display_personal_page']
-    search_fields = ['display_name','address_line_1', 'parish', 'bio']
+    list_display = [
+        'display_name', 'bio',
+        'image_file', 'shop_web_address', 
+        'outlets', 'listed_member',
+        'display_personal_page', 'address_line_1',
+        'address_line_2', 'parish',
+        'postcode', 'avatar',
+        'rules_agreed'
+    ]
+    list_filter = [
+        'display_name', 'parish',
+        'rules_agreed', 'shop_web_address',
+        'listed_member', 'display_personal_page'
+    ]
+    search_fields = [
+        'display_name', 'address_line_1',
+        'parish', 'bio'
+    ]
 
     def get_queryset(self, request):
         return super().get_queryset(request).exclude(profile_user__is_superuser=True)
@@ -108,7 +146,7 @@ class tasks:
             logger.error("Dropbox HttpError : {0}".format(e))
         for entry in dbx.files_list_folder('', recursive=True).entries:
             dbx.files_delete(entry.path_display)
-        management.call_command("dbbackup")
+        management.call_command("dbbackup --traceback")
         management.call_command("mediabackup")
         logger.info("succesfully backed up database and media files")
 
